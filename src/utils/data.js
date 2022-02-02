ea10a4285d6850c2ebc00a02af8afced9379c243
Jibbyworks <JDBucog@yahoo.com>
2022-02-01 19:58:29 -0800

    add new comps

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,14 +1,14 @@
-const data = {
-  product_image: [
-    "../images/image-product-1.jpg",
-    "../images/image-product-2.jpg",
-    "../images/image-product-3.jpg",
-    "../images/image-product-4.jpg",
-  ],
-  product_image_thumbnail: [
-    "../images/image-product-1-thumbnail.jpg",
-    "../images/image-product-2-thumbnail.jpg",
-    "../images/image-product-3-thumbnail.jpg",
-    "../images/image-product-4-thumbnail.jpg",
-  ]
+import thumb1 from "../images/image-product-1-thumbnail.jpg"
+import thumb2 from "../images/image-product-2-thumbnail.jpg"
+import thumb3 from "../images/image-product-3-thumbnail.jpg"
+import thumb4 from "../images/image-product-4-thumbnail.jpg"
+
+import image1 from "../images/image-product-1.jpg"
+import image2 from "../images/image-product-2.jpg"
+import image3 from "../images/image-product-3.jpg"
+import image4 from "../images/image-product-4.jpg"
+
+export const data = {
+  product_image: [image1, image2, image3, image4],
+  product_image_thumbnail: [thumb1, thumb2, thumb3, thumb4],
 }
